a12acc15cb91a34b85b4786201a26047d1714919
Mstovall4 <mlsdn6@umsystem.edu>
2024-10-23 22:55:29 -0400

    Updated duckdns-update.sh with new domain name

--- duckdns-update.sh
+++ duckdns-update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set your Duck DNS token and domain
-TOKEN="517bc219-6fe9-4129-b4eb-5474de57a3f7"  
-DOMAIN="test-server-1"  
+TOKEN="38f6bfcc-f9d0-483f-95b2-f3d61502dbbd"  
+DOMAIN="test-domain1"  
 
 # Get the current public IP address
 IP=$(curl -s http://ipv4.icanhazip.com)
